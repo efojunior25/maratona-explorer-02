e3fc50a0d4ed6fc951f8284ce32282809a3e41ec
Edson Júnior <efojunior25@gmail.com>
2022-10-11 15:55:25 -0300

    Adicionando botão de temas

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const switcher = document.querySelector('.button');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('pink')
+
+    var className = document.body.className;
+    if(className == "dark-blue"){
+        this.textContent = "Tema Rosa";
+    } else {
+        this.textContent = "Tema Azul";
+    }
+
+    console.log('Nome da classe atual: ' + className);
+});
